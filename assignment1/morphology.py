f6f76a8cb867e3f2301bf1efb6f24bc9cf1bea01
Femi Coder <adegele43@gmail.com>
2026-02-27 12:13:50 +0000

    added manual dilatoon operation

---
+++ assignment1/morphology.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def dilation(binary_img):
+    rows, cols = binary_img.shape
+    output = np.zeros_like(binary_img, dtype=np.uint8)
+    
+    for i in range(1, rows - 1):
+        for j in range(1, cols - 1):
+            
+            # if any pixel in the 3 x 3 neighbourhood is white - 255
+            if np.any(binary_img[i-1:i+2, j-1:j+2] == 255):
+                output[i, j] = 255
+            else:
+                output[i, j] = 0
+    return output 
